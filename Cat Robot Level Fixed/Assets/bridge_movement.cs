ddc67790b8c6c88f5bd2e7f16a35763294485c52
areitmeyer <41389057+areitmeyer@users.noreply.github.com>
2018-11-14 13:29:14 -0500

    unity project

---
+++ Cat Robot Level Fixed/Assets/bridge_movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bridge_movement : MonoBehaviour 
+{
+	public GameObject empty;
+
+	public void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "Player") 
+		{
+			gameObject.transform.position = empty.transform.position;
+			gameObject.transform.rotation = Quaternion.AngleAxis(0f, Vector3.right);
+			gameObject.transform.rotation = Quaternion.AngleAxis(-92f, Vector3.up);
+		//	gameObject.transform.rotation = Quaternion.AngleAxis(0f, Vector3.forward);
+		}
+	}
+}
